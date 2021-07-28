0cc856573ea280d1ff806e85456234cd89ce2a40
AppreciativeGeek <mnpencildoodle@gmail.com>
2021-07-28 12:09:21 -0500

    Created AssignmentItem custom struct. Create assignmentItems array.

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems = [AssignmentItem(course: "Math", description: "Finish Assignment", dueDate: Date()),
+                              AssignmentItem(course: "Reading", description: "Read 10 pages of a book.", dueDate: Date()),
+                              AssignmentItem(course: "Science", description: "Follow Experient Instructions", dueDate: Date())]
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -18,4 +22,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct AssignmentItem : Identifiable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
